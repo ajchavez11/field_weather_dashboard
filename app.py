8400e2e34062e8ee831a4d64b1a16933060142b9
ajchavez11 <adamjchavez11@gmail.com>
2025-08-14 17:06:51 -0800

    Finished api call and json loading

--- app.py
+++ app.py
@@ -21,19 +21,31 @@ if fetch_button:
         lat_float = float(lat)
         lon_float = float(lon)
         st.write(f'Valid Coordinates: {lat_float}, {lon_float}')
+        url = 'https://api.openweathermap.org/data/2.5/forecast'
+        params = {
+            'lat': lat_float,
+            'lon': lon_float,
+            'appid': api_key,
+            'units': 'metric',
+        }
+
+        cache_path = 'data/cache.json'
+        if os.path.exists(cache_path):
+            with open(cache_path, 'r') as f:
+                data = json.load(f)
+                st.info(f'Cache file {cache_path} loaded')
+                st.write(f'Cache data for {data['city']['name']}')
+                st.success(f'Got weather data! Ready to process!')
+        else:
+            response = r.get(url, params=params)
+            if response.status_code == 200:
+                data = response.json()
+                cache_path = 'data/cache.json'
+                with open(cache_path, 'w') as f:
+                    json.dump(data, f)
+            else:
+                st.error(f'API Error: {response.json().get('message', 'Unknown Error')}')
     except ValueError:
         st.error('Please enter a valid latitude and longitude.')
-
-
-cache_path = 'data/cache.json'
-
-if os.path.exists(cache_path):
-    with open(cache_path, 'r') as f:
-        cache = json.load(f)
-        st.info('Loaded from Cache File')
-        with open(cache_path, 'w') as f:
-            json.dump(cache, f)
-else:
-    st.error('Cache File Not Found')
 
 
